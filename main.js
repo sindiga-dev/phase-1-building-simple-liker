eee64fe365b3dbd8646a48436e36dd03364e00cb
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-28 21:16:51 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
+const modal = document.getElementById("modal");
+modal.className = "hidden";
 
+let glyphs = document.querySelectorAll(".like-glyph");
+glyphs.forEach((glyph) => {
+	glyph.addEventListener("click", () => {
+		mimicServerCall()
+			.then(() => {
+				if (glyph.innerHTML === EMPTY_HEART) {
+					glyph.innerHTML = FULL_HEART;
+					glyph.className = "activated-heart";
+				} else {
+					glyph.innerHTML = EMPTY_HEART;
+					glyph.className = "";
+				}
+			})
 
-
+			.catch((error) => {
+				modal.className = "";
+				modal.innerText = error;
+				setTimeout(() => {
+					modal.className = "hidden";
+				}, 3000);
+			});
+	});
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+	return new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			let isRandomFailure = Math.random() < 0.2;
+			if (isRandomFailure) {
+				reject("Random server error. Try again.");
+			} else {
+				resolve("Pretend remote server notified of action!");
+			}
+		}, 300);
+	});
 }
